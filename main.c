a24b086fd39e4bfbf25a3da33f06559118df9749
3mr <amramr9016@gmail.com>
2023-12-15 09:10:44 -0800

    DONE

--- main.c
+++ main.c
@@ -27,7 +27,7 @@ int main(int ac, char **argv)
 		if (!command)
 			continue;
 
-		for (i = 0; comand[i]; i++)
+		for (i = 0; command[i]; i++)
 			printf("%s\n", command[i]);
 
 		/*status = _execute(command, argv);*/
